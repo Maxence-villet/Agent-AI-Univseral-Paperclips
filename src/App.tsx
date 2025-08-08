cf2c076340317ead601bd65642ebc1693cf23808
Maxence-villet <maxence.villetschoumaker@gmail.com>
2025-08-08 05:14:52 +0200

    Refactor App component to remove routing and directly render ChartPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import ChartPage from './app/chartPage/page';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<ChartPage />} />
-      </Routes>
-    </Router>
+    <ChartPage />
   );
 }
 
